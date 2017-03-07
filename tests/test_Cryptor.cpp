d70a0da5e16ab8a614db8a6153ae17d43399006d
Farid Hajji <farid@hajji.name>
2017-03-07 03:35:51 +0100

    Added test fixture for each individual test case.

--- tests/test_Cryptor.cpp
+++ tests/test_Cryptor.cpp
@@ -30,20 +30,39 @@ test_of_correctness(std::string &plaintext)
   return plainblob == decrypted;
 }
 
-BOOST_AUTO_TEST_SUITE ( sodium_test_suite );
+struct SodiumFixture {
+  SodiumFixture()  {
+    BOOST_REQUIRE(sodium_init() != -1);
+    BOOST_TEST_MESSAGE("SodiumFixture(): sodium_init() successful.");
+  }
+  ~SodiumFixture() {
+    BOOST_TEST_MESSAGE("~SodiumFixture(): teardown -- no-op.");
+  }
+};
+
+/**
+ * The previous fixture is RAII called _for each_ test case
+ * individually; i.e. sodium_init() is initialized multiple times.
+ *
+ * If you prefer to to this fixture only once for the whole test
+ * suite, replace BOOST_FIXTURE_TEST_SUITE (...) by call call to
+ * BOOST_AUTO_TEST_SUITE (sodium_test_suite,
+ *                        * boost::unit_test::fixture<SodiumFixture>())
+ * i.e. using decorators. 
+ * 
+ * To see the output of the messages, invoke with --log_level=message.
+ **/
+
+BOOST_FIXTURE_TEST_SUITE ( sodium_test_suite, SodiumFixture );
 
 BOOST_AUTO_TEST_CASE( sodium_cryptor_test_full_plaintext )
 {
-  BOOST_REQUIRE(sodium_init() != -1);
-
   std::string plaintext {"the quick brown fox jumps over the lazy dog"};
   BOOST_CHECK(test_of_correctness(plaintext));
 }
 
 BOOST_AUTO_TEST_CASE( sodium_cryptor_test_empty_plaintext )
 {
-  BOOST_REQUIRE(sodium_init() != -1);
-
   std::string plaintext {};
   BOOST_CHECK(test_of_correctness(plaintext));
 }
